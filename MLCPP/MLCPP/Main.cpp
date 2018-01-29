45e0cc5cfd0d6810a3702ba92868de3bc5fea327
Tajson999 <alexanderljungberg@hotmail.com>
2018-01-29 08:15:19 +0100

    stuff

--- MLCPP/MLCPP/Main.cpp
+++ MLCPP/MLCPP/Main.cpp
@@ -1,27 +1,43 @@
 #include <iostream>
 #include <fstream>
 
+
 using namespace std;
 
 int main() {
 
 	fstream file;
-	file.open("download.png", ios::in, ios::binary);
-
-	int min = file.tellg();
+	//path: C:\\Users\\alexa\\Desktop\\ML_CPP\\train-images.idx3-ubyte
+	file.open("C:\\Users\\alexa\\Desktop\\ML_CPP\\train-images.idx3-ubyte", ios::in, ios::binary);
+	int min, max, bytes;
+	min = file.tellg();
 	file.seekg(0, ios::end);
-	int max = file.tellg();
+	max = file.tellg();
 	file.seekg(0, ios::beg);
+	bytes = max - min;
+	cout << "bytes is " << bytes << endl;
+	char* buffer = new char(bytes);
+	file.read(buffer, bytes);
 
-	int bytes = max - min;
-	cout << "Bytes is " << bytes << endl
-	char* buffer = new char(4096);
-	file.read(buffer, sizeof(buffer));
-	for (int i = 0; i < sizeof(4096); i++) {
-		cout << *(buffer + i);
-	}
+
+	int magicNumber = (int)buffer;
+	cout << "Magic number is " << magicNumber << endl;
+
+	int *nrImages;
+	nrImages = (int *)buffer;
+	cout << "nrImages is " << nrImages << endl;
+
+	int nrRows;
+	file.read((char *)&nrRows, sizeof(int));
+	cout << "nrRows is " << nrRows << endl;
+
+	int nrCols;
+	file.read((char *)&nrCols, sizeof(int));
+	cout << "nrCols is " << nrCols << endl;
 
 	getchar();
+
+	delete[] buffer;
 
 	return 0;
 }
